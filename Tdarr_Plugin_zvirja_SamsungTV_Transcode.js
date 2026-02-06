256216308734febd87dbdb0edaff6a7286de7550
Oleksandr Povar <1074182+zvirja@users.noreply.github.com>
2026-02-06 04:17:16 +0100

    feat: Allow to remove unwanted languages

--- Tdarr_Plugin_zvirja_SamsungTV_Transcode.js
+++ Tdarr_Plugin_zvirja_SamsungTV_Transcode.js
@@ -12,15 +12,13 @@ const details = () => {
     Inputs: [
       {
         name: "preferred_language",
-        type:'string',
-        defaultValue:'eng',
+        type: 'string',
+        defaultValue: 'eng',
         inputUI: {
           type: 'text',
         },
         tooltip: `Specify one language tag for Tdarr to try and put as 1st audio & substitle track
-
         \\nExample:\\n
-
         eng 
         `,
       },
@@ -33,8 +31,6 @@ const details = () => {
         },
         tooltip: `Specifiy the codecs which you'd like to transcode
         \\nExample:\\n
-        ac3
-        \\nExample:\\n
         eac3,ac3,aac
         `,
       },
@@ -46,16 +42,20 @@ const details = () => {
           type: 'text',
         },
         tooltip: `Specify the codec you'd like to transcode into:
-        \\n aac
-        \\n ac3
-        \\n eac3
-        \\n dts
-        \\n flac
-        \\n mp2
-        \\n mp3
-        \\n truehd
         \\nExample:\\n
         eac3
+        `,
+      },
+      {
+        name: "langs_to_keep",
+        type: 'string',
+        defaultValue: '',
+        inputUI: {
+          type: 'text',
+        },
+        tooltip: `If specified, only configured languages will be left for audio and subtitles:
+        \\nExample:\\n
+        eng,ukr
         `,
       }
     ],
@@ -88,6 +88,7 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
   const preferred_language = inputs.preferred_language;
   const audio_codecs_to_transcode = inputs.audio_codecs_to_transcode.split(",");
   const audio_target_codec = inputs.audio_target_codec;
+  const languages_to_keep = inputs.langs_to_keep?.split(",");
 
   let requireProcessing = false;
   let requireProcessingInfo = "Requires processing:"
@@ -104,7 +105,7 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
     ) {
       if (i > 0) {
         requireProcessing = true;
-        requireProcessingInfo += " audio_stream_position"
+        requireProcessingInfo += ` audio_stream_position[${stream.index}->0]`
 
         // Put track as first
         const [preferredTrack] = audioStreams.splice(i, 1);
@@ -127,7 +128,7 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
     ) {
       if (i > 0) {
         requireProcessing = true;
-        requireProcessingInfo += " subtitle_stream_position"
+        requireProcessingInfo += ` subtitle_stream_position[${stream.index}->0]`
 
         // Put track as first
         const [preferredTrack] = subtitleStreams.splice(i, 1);
@@ -145,11 +146,17 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
   for (let i = 0; i < audioStreams.length; ++i) {
     const stream = audioStreams[i];
 
+    if (!!languages_to_keep && !!stream.tags?.language && !languages_to_keep.includes(stream.tags.language.toLowerCase()) {
+      requireProcessing = true;
+      requireProcessingInfo += ` audio_stream_skip_by_lang[${stream.index},${stream.tags.language}]`
+      continue;
+    }
+
     if (!!stream.codec_name && audio_codecs_to_transcode.includes(stream.codec_name.toLowerCase())) {
       ffmpegCommand += `-map 0:${stream.index} -c:a:${i} ${audio_target_codec} `;
 
       requireProcessing = true;
-      requireProcessingInfo += " audio_stream_codec"
+      requireProcessingInfo += ` audio_stream_codec[${stream.index},${stream.codec_name}->${audio_target_codec}]`
     } else {
       ffmpegCommand += `-map 0:${stream.index} `;
     }
@@ -164,6 +171,12 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
   // Lay subtitle streams
   for (let i = 0; i < subtitleStreams.length; ++i) {
     const stream = subtitleStreams[i];
+
+    if (!!languages_to_keep && !!stream.tags?.language && !languages_to_keep.includes(stream.tags.language.toLowerCase()) {
+      requireProcessing = true;
+      requireProcessingInfo += ` subtitle_stream_skip_by_lang[${stream.index},${stream.tags.language}]`
+      continue;
+    }
 
     ffmpegCommand += `-map 0:${stream.index} `;
 
